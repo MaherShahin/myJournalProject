36e282c6096e442ee358fa1e666a63cfcf04c0ff
MaherShahin <abdomaher2010@hotmail.com>
2022-09-14 21:29:36 +0200

    login component replace with react-bootstrap

--- public/react-frontend/src/components/login/login.js
+++ public/react-frontend/src/components/login/login.js
@@ -4,62 +4,40 @@ import "../../styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button, Form, FormGroup, Label, Input } from "react-bootstrap";
 
 export default class Login extends Component {
+  onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="container d-flex justify-content-md-center align-items-center vh-100">
         <div className="row">
           <div className="col">
             <h1>Your journey starts here!</h1>
+            <Form onSubmit={this.onSubmit}>
+              <Form.Group controlId="formBasicUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="username" placeholder="Enter username" />
+              </Form.Group>
 
-            <form
-              className="form-signin"
-              method="post"
-              action="/login"
-              id="form-signin"
-            >
-              <h2 className="form-signin-heading text-center">Please sign in</h2>
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
 
-              <label for="username" className="font-weight-bold">
-                Username
-              </label>
-              <input
-                name="username"
-                type="username"
-                id="username"
-                className="form-control"
-                placeholder="Username"
-                required=""
-
-              />
-
-              <label for="password" class="my-1 font-weight-bold">
-                Password
-              </label>
-              <input
-                name="password"
-                type="password"
-                id="password"
-                className="form-control"
-                placeholder="Password"
-                required=""
-              />
-
-              <div className="text-center">
-                <button
-                  className="btn btn-lg btn-dark btn-block text-center my-2"
-                  type="submit"
-                >
-                  Sign in
-                </button>
-                <br />
-                <h5>Don't have an account yet? Register with us</h5>
-                <Link to="/register" class="btn btn-dark my-1" role="button">
+              <div className="d-flex justify-content-center flex-column my-3">
+                <Button className="w-25 align-self-center" variant="dark" type="submit">
+                  Login
+                </Button>
+                <h5 className="align-self-center my-3"> No account yet? Then click here to join us!</h5>
+                <Button className="w-25 align-self-center"  variant="dark" as={Link} to="/register">
                   Register
-                </Link>
+                </Button>
               </div>
-            </form>
+            </Form>
           </div>
         </div>
       </div>
