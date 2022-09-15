484644847929d9ce6082d48ac1ae530add211b4e
MaherShahin <abdomaher2010@hotmail.com>
2022-09-15 05:26:54 +0200

    removed unnecessary log form auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,6 @@ const jwt = require("express-jwt");
 const getTokenFromCookies = (req) => {
   const userJSON = req.cookies.user;
   // decode the token from the cookie
-  console.log(userJSON); // TO-DO -> remove this line
-  // When on JournalEntries, getTokenFromCookies is called infinite times, probably due to the get request to /journalEntries
   if (userJSON) {
     return userJSON.token;
   }
