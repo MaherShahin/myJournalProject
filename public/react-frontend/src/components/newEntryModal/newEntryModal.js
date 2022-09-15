bc3af35aa4adfa7aa549eb82a3cd67d9c0fa4b89
MaherShahin <abdomaher2010@hotmail.com>
2022-09-15 18:44:11 +0200

    sizing new entry modal

--- public/react-frontend/src/components/newEntryModal/newEntryModal.js
+++ public/react-frontend/src/components/newEntryModal/newEntryModal.js
@@ -6,37 +6,53 @@ export default class newEntryModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        entry : {
-            title: "",
-            content: "",
-        }
+      entry: {
+        title: "",
+        content: "",
+      },
     };
   }
 
-  
   render() {
     return (
-      <Modal show={this.props.newEntry} onHide={this.props.closeModal} style={{color:"black"}} >
+      <Modal
+        show={this.props.newEntry}
+        onHide={this.props.closeModal}
+        style={{ color: "black", width: "90%", maxWidth: "none" }}
+        aria-labelledby="example-modal-sizes-title-xl"
+        dialogClassName="modal-90w"
+      >
         <Modal.Header closeButton>
           <Modal.Title>New Entry</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group controlId="formBasicEmail">
+            <Form.Group>
               <Form.Label>Title</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter title"
-                onChange={(e) => this.setState({ ...this.state, entry: { ...this.state.entry, title: e.target.value } })}
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    entry: { ...this.state.entry, title: e.target.value },
+                  })
+                }
               />
             </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
+            <Form.Group>
               <Form.Label>Content</Form.Label>
               <Form.Control
-                rows="10"
+                as="textarea"
+                rows={20}
                 placeholder="Enter content"
-                onChange={(e) => this.setState({...this.state, entry: { ...this.state.entry, content: e.target.value } })}
+                onChange={(e) =>
+                  this.setState({
+                    ...this.state,
+                    entry: { ...this.state.entry, content: e.target.value },
+                  })
+                }
               />
             </Form.Group>
           </Form>
