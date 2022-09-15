55804c8b1db2ccb9f78407007e943b62b595983a
MaherShahin <abdomaher2010@hotmail.com>
2022-09-15 18:34:04 +0200

    colors of new entry button

--- public/react-frontend/src/components/newEntryButton/newEntryButton.js
+++ public/react-frontend/src/components/newEntryButton/newEntryButton.js
@@ -12,15 +12,15 @@ export default class NewEntryButton extends React.Component {
         <IoAddCircle
           onClick={() => this.props.handleNewEntry()}
           size={100}
-          style={{ color: "green", marginRight: "20px" }}
+          style={{ color: "grey", marginRight: "20px" }}
           onMouseEnter={(e) => {
-            e.currentTarget.style.color = "white";
+            e.currentTarget.style.color = "green";
             e.currentTarget.style.cursor = "pointer";
             e.currentTarget.style.transform = "scale(1.1)";
             e.currentTarget.style.transition = "0.5s";
           }}
           onMouseLeave={(e) => {
-            e.currentTarget.style.color = "green";
+            e.currentTarget.style.color = "grey";
             e.currentTarget.style.transform = "scale(1)";
             e.currentTarget.style.transition = "0.5s";
           }}
